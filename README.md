3429f2b4521cbfd8866dcdb9f110a41853bf8831
Денис <d.rusakov@webpractik.ru>
2024-03-11 16:12:32 +0300

    feat(presentation): initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Презентация «TailwindCSS. Попутный ветер перемен»
+
+<img src="./pictures/logo.svg" width="250" height="120" alt="Логотип">
---
+++ tailwind.config.ts
@@ -0,0 +1,63 @@
+import type { Config } from 'tailwindcss';
+
+module.exports = {
+  content: ['./index.html'],
+  darkMode: 'selector',
+  theme: {
+    screens: {
+      fullWidth: '1920px',
+      bigDesktop: '1600px',
+      desktopLg: '1399px',
+      desktop: '1024px',
+      tablet: '991px',
+      mobile: '767px',
+      mobileSm: '576px',
+      minWidth: '320px',
+    },
+    colors: {
+      white: '#fff',
+      purple: '#7927e0',
+      blue: '#38bdf8',
+      red: '#ff0000',
+      'pink-dark': '#2b213a',
+      'purple-dark': '#0f172a',
+      'neon-cyan': 'oklch(91.3% 0.139 195.8)',
+    },
+    fontFamily: {
+      oswald: 'Oswald, sans-serif',
+    },
+    extend: {
+      animation: {
+        'fly-logo':
+          'fly-x var(--fly-duration-x) linear infinite alternate, fly-y var(--fly-duration-y) linear infinite alternate, fly-color-x calc(var(--fly-duration-x) * 5) step-start infinite, fly-color-y calc(var(--fly-duration-y) * 5) step-start infinite',
+      },
+      keyframes: {
+        'fly-x': {
+          from: { left: '0' },
+          to: { left: 'calc(100% - var(--fly-logo-w))' },
+        },
+        'fly-y': {
+          from: { top: '0' },
+          to: { top: 'calc(100% - var(--fly-logo-h))' },
+        },
+        'fly-color-x': {
+          '0%': { '--fly-color-x': '0' },
+          '20%': { '--fly-color-x': '2' },
+          '40%': { '--fly-color-x': '4' },
+          '60%': { '--fly-color-x': '1' },
+          '80%': { '--fly-color-x': '3' },
+          '100%': { '--fly-color-x': '0' },
+        },
+        'fly-color-y': {
+          '0%': { '--fly-color-y': '0' },
+          '20%': { '--fly-color-y': '2' },
+          '40%': { '--fly-color-y': '4' },
+          '60%': { '--fly-color-y': '1' },
+          '80%': { '--fly-color-y': '3' },
+          '100%': { '--fly-color-y': '0' },
+        },
+      },
+    },
+  },
+  important: '.shower',
+} satisfies Config;
